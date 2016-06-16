07b2401f08dbe54ae27b3812742c1ff8062b0e44
Markus Andresen <markua@stud.ntnu.no>
2016-06-16 14:33:22 +0200

    Deleted lines to test heroku deployment

--- routes/sessions.js
+++ routes/sessions.js
@@ -4,6 +4,13 @@
 var express = require('express');
 var router = express.Router();
 
+var pg = require('pg');
+var DATABASE_URL = "postgres://bnzoxupkdsqepi:w7Ds13XvOYlQlhZW4KB8o7PRyb@ec2-54-83-25-238.compute-1.amazonaws.com:5432/da2rmgi8pj04e1"
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+});
 
 function getConnection(){
   var mysql = require('mysql')
@@ -34,165 +41,10 @@ function filterWithParameters(array,params){
 
 
 router.get('/', function(req, res, next) {
-  connection = getConnection();
-  connection.query('select sessionid,sessions.personid_fk,date,exercise_name, kg, reps from sessions '+
-  'join results on sessionid_fk=sessionid join exercise on exerciseid = exerciseid_fk '+
-  'join execution on resultid=resultid_fk order by date desc', function(err,result){
-    if(err){
-      console.error(err);
-      res.send(500);
-      return;
-    }
-    var identifier = ""
-    var identifier2 = ""
-    var objects =[]
-    var object = { }
-    var first = false;
-    for(i=0;i<result.length;i++){
-      if(result[i].place+result[i].date != identifier){
-        objects.push(object);
-        object = {};
-        object.id = result[i].sessionid;
-        object.user = result[i].personid_fk;
-        object.place = result[i].place;
-        object.date = result[i].date;
-        object.exercises = [{ name: result[i].exercise_name, sets: [{kg: result[i].kg, reps: result[i].reps}]}];
-        first = true
-      }
-      else{
-        if(identifier2 != result[i].date+result[i].exercise_name){
-          if(!first){
-          object.exercises.push({ name: result[i].exercise_name, sets: [{kg: result[i].kg, reps: result[i].reps}]})
-        }
-        else{
-          object.exercises[object.exercises.length-1].sets.push({kg: result[i].kg, reps: result[i].reps})
-        }
-        }
-        else{
-            object.exercises[object.exercises.length-1].sets.push({kg: result[i].kg, reps: result[i].reps})
-        }
-        identifier2 = result[i].date+result[i].exercise_name
-        first = false;
-      }
-      identifier = result[i].place+result[i].date
-    }
-
-    objects.shift()
-    objects = filterWithParameters(objects,req.query)
     res.writeHead(200, {"Content-Type": "application/json"});
-    res.end(JSON.stringify({response: "success", rows: objects.length, data: objects }));
+    res.end(JSON.stringify({response: "success"}));
     connection.end();
-  });
 });
-
-
-router.get('/:id', function(req, res, next) {
-  connection = getConnection();
-  connection.query('select sessionid,sessions.personid_fk,date,exercise_name, kg, reps from sessions '+
-  'join results on sessionid_fk=sessionid join exercise on exerciseid = exerciseid_fk '+
-  'join execution on resultid=resultid_fk order by date desc', function(err,result){
-    if(err){
-      console.error(err);
-      res.send(500);
-      return;
-    }
-    var identifier = ""
-    var identifier2 = ""
-    var objects =[]
-    var object = { }
-    var first = false;
-    for(i=0;i<result.length;i++){
-      if(result[i].place+result[i].date != identifier){
-        objects.push(object);
-        object = {};
-        object.id = result[i].sessionid;
-        object.user = result[i].personid_fk;
-        object.place = result[i].place;
-        object.date = result[i].date;
-        object.exercises = [{ name: result[i].exercise_name, sets: [{kg: result[i].kg, reps: result[i].reps}]}];
-        first = true
-      }
-      else{
-        if(identifier2 != result[i].date+result[i].exercise_name){
-          if(!first){
-          object.exercises.push({ name: result[i].exercise_name, sets: [{kg: result[i].kg, reps: result[i].reps}]})
-        }
-        else{
-          object.exercises[object.exercises.length-1].sets.push({kg: result[i].kg, reps: result[i].reps})
-        }
-        }
-        else{
-            object.exercises[object.exercises.length-1].sets.push({kg: result[i].kg, reps: result[i].reps})
-        }
-        identifier2 = result[i].date+result[i].exercise_name
-        first = false;
-      }
-      identifier = result[i].place+result[i].date
-    }
-
-    objects.shift()
-    console.log(req.params.id)
-    objects = filterWithParameters(objects,{id: req.params.id})
-    res.writeHead(200, {"Content-Type": "application/json"});
-    res.end(JSON.stringify({response: "success", rows: objects.length, data: objects }));
-    connection.end();
-  });
-});
-
-router.get('/user/:id', function(req, res, next) {
-  connection = getConnection();
-  connection.query('select sessionid,sessions.personid_fk,date,exercise_name, kg, reps from sessions '+
-  'join results on sessionid_fk=sessionid join exercise on exerciseid = exerciseid_fk '+
-  'join execution on resultid=resultid_fk order by date desc', function(err,result){
-    if(err){
-      console.error(err);
-      res.send(500);
-      return;
-    }
-    var identifier = ""
-    var identifier2 = ""
-    var objects =[]
-    var object = { }
-    var first = false;
-    for(i=0;i<result.length;i++){
-      if(result[i].place+result[i].date != identifier){
-        objects.push(object);
-        object = {};
-        object.id = result[i].sessionid;
-        object.user = result[i].personid_fk;
-        object.place = result[i].place;
-        object.date = result[i].date;
-        object.exercises = [{ name: result[i].exercise_name, sets: [{kg: result[i].kg, reps: result[i].reps}]}];
-        first = true
-      }
-      else{
-        if(identifier2 != result[i].date+result[i].exercise_name){
-          if(!first){
-          object.exercises.push({ name: result[i].exercise_name, sets: [{kg: result[i].kg, reps: result[i].reps}]})
-        }
-        else{
-          object.exercises[object.exercises.length-1].sets.push({kg: result[i].kg, reps: result[i].reps})
-        }
-        }
-        else{
-            object.exercises[object.exercises.length-1].sets.push({kg: result[i].kg, reps: result[i].reps})
-        }
-        identifier2 = result[i].date+result[i].exercise_name
-        first = false;
-      }
-      identifier = result[i].place+result[i].date
-    }
-
-    objects.shift()
-    console.log(req.params.id)
-    objects = filterWithParameters(objects,{user: req.params.id})
-    res.writeHead(200, {"Content-Type": "application/json"});
-    res.end(JSON.stringify({response: "success", rows: objects.length, data: objects }));
-    connection.end();
-  });
-});
-
-
 
 
 
